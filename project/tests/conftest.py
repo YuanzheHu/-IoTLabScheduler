816ced02d01d23337ae12ea29e0fe49b38facf2a
Yuanzhe Hu <steven_ghjq@163.com>
2025-07-16 23:29:15 +0100

    🧪 test: update test

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from starlette.testclient import TestClient
+import logging
+import sys
 
 from main import app
 
@@ -8,3 +10,18 @@ from main import app
 def test_app():
     client = TestClient(app)
     yield client  # testing happens here
+
+
+def pytest_configure(config):
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filename='tests/test.log',
+        filemode='a'
+    )
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
